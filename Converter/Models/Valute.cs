fa25fd689f6ec65a677e02cc08c548630b409be7
Mark-Shnaider <shnaJderMARCK@yandex.ru>
2021-04-23 17:45:08 +0700

    Добавьте файлы проекта.

---
+++ Converter/Models/Valute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Models
+{
+    class Valute
+    {
+        public int Id { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode {get;set;}
+        public int Nominal { get; set; }
+        public double Value { get; set; }
+        public double Previous { get; set; }
+
+        
+    }
+}
